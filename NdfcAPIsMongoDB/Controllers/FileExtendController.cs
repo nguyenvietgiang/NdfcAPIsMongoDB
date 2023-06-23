b12d060b4ae328c2c41a38d13a9e2bd644c0d83c
Giang <nguyenvietgiang1110@gmail.com>
2023-06-23 11:15:09 +0700

    Hoàn thiện các APIs portal

--- NdfcAPIsMongoDB/Controllers/FileExtendController.cs
+++ NdfcAPIsMongoDB/Controllers/FileExtendController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using NdfcAPIsMongoDB.FileService;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf;
+using Syncfusion.Drawing;
 
 namespace NdfcAPIsMongoDB.Controllers
 {
@@ -61,6 +64,47 @@ namespace NdfcAPIsMongoDB.Controllers
             }
 
             return File(templateBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", templateName + ".xlsx");
+        }
+
+        [HttpGet("pdf-invitation")]
+        public IActionResult GenerateInvitation(string name, DateTime time, string reason)
+        {
+            // Tạo một đối tượng PDF mới
+            using (PdfDocument document = new PdfDocument())
+            {
+                // Tạo một trang mới
+                PdfPage page = document.Pages.Add();
+                PdfGraphics graphics = page.Graphics;
+                PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+                float lineSpacing = font.Height * 1.2f;
+
+                string text = "Meeting invitation";
+                SizeF textSize = font.MeasureString(text);
+                float textX = (page.GetClientSize().Width - textSize.Width) / 2;
+                float textY = 80;
+                graphics.DrawString(text, font, PdfBrushes.Black, textX, textY);
+
+                float bodyY = page.GetClientSize().Height - lineSpacing * 2;
+                graphics.DrawString("Dear: " + name, new PdfStandardFont(PdfFontFamily.Helvetica, 12), PdfBrushes.Black, 50, bodyY);
+                bodyY -= lineSpacing;
+                graphics.DrawString("At " + time, new PdfStandardFont(PdfFontFamily.Helvetica, 12), PdfBrushes.Black, 50, bodyY);
+                bodyY -= lineSpacing;
+                graphics.DrawString("To: " + reason, new PdfStandardFont(PdfFontFamily.Helvetica, 12), PdfBrushes.Black, 50, bodyY);
+
+                PdfFont namDinhTodayFont = new PdfStandardFont(PdfFontFamily.Helvetica, 10);
+                string namDinhTodayText = "NamDinh / " + DateTime.Today.ToString("yyyy-MM-dd");
+                SizeF namDinhTodaySize = namDinhTodayFont.MeasureString(namDinhTodayText);
+                float namDinhTodayX = (page.GetClientSize().Width - namDinhTodaySize.Width) / 2;
+                bodyY -= lineSpacing;
+                graphics.DrawString(namDinhTodayText, namDinhTodayFont, PdfBrushes.Black, namDinhTodayX, bodyY);
+
+                // Lưu file PDF vào một MemoryStream
+                MemoryStream stream = new MemoryStream();
+                document.Save(stream);
+                stream.Position = 0;
+                // Trả về file PDF như là một phản hồi HTTP
+                return File(stream, "application/pdf", "invitation.pdf");
+            }
         }
     }
 }
